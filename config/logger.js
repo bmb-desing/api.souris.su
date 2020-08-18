aa7c52004769eb8fc655ccec2b550cfbbb309c75
bmb-design92 <35960774+xolms111@users.noreply.github.com>
2020-08-18 17:52:18 +0300

    123

--- config/logger.js
+++ config/logger.js
@@ -1,15 +1,14 @@
-const dotenv = require('dotenv')
-const winston = require('winston')
+const dotenv = require("dotenv");
+const winston = require("winston");
 
-dotenv.config()
+dotenv.config();
 
 function myTimestamp() {
   return new Date().toString();
-};
-
+}
 
 const logger = winston.createLogger({
-  level: 'info', 
+  level: "info",
   format: winston.format.json(),
   exitOnError: false,
   transports: [
@@ -17,40 +16,43 @@ const logger = winston.createLogger({
     // - Write all logs with level `error` and below to `error.log`
     // - Write all logs with level `info` and below to `combined.log`
     //
-    new winston.transports.File(
-      { 
-        filename: './system/logs/error.log',
-        level: 'error',
-        maxsize: 5242880, //5MB
-        maxFiles: 5,
-        handleExceptions: true,
-        colorize: false,
-        timestamp: myTimestamp
-      }
-    ),
-    new winston.transports.File(
-      {
-        filename: './system/logs/logs.log',
-        handleExceptions: true,
-        json: false,
-        maxsize: 5242880, //5MB
-        maxFiles: 5,
-        colorize: false,
-        timestamp: myTimestamp
-      }
-    ),
+    new winston.transports.File({
+      filename: "./system/logs/error.log",
+      level: "error",
+      maxsize: 5242880, //5MB
+      maxFiles: 5,
+      handleExceptions: true,
+      colorize: false,
+      timestamp: myTimestamp,
+    }),
+    new winston.transports.File({
+      filename: "./system/logs/logs.log",
+      handleExceptions: true,
+      json: false,
+      maxsize: 5242880, //5MB
+      maxFiles: 5,
+      colorize: false,
+      timestamp: myTimestamp,
+    }),
   ],
-})
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }));
+});
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+      handleExceptions: true,
+      json: false,
+      maxsize: 5242880, //5MB
+      maxFiles: 5,
+      colorize: false,
+    })
+  );
 }
 logger.stream = {
-  write: function(message, encoding) {
+  write: function (message, encoding) {
     // use the 'info' log level so the output will be picked up by both transports (file and console)
     logger.info(message);
   },
 };
 
-module.exports = logger
+module.exports = logger;
